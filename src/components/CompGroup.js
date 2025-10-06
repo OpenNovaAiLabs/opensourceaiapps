8a69b7cc858ccaefbd739cd53373d7d5823e55c5
OpenNovaAiLabs <jamescheriyanco@gmail.com>
2025-10-06 09:39:15 +0100

    Update CompGroup.js

--- src/components/CompGroup.js
+++ src/components/CompGroup.js
@@ -1,63 +1,84 @@
-import React, { useState, useEffect } from "react"
-import { Box, Divider, Link, Tooltip } from "@chakra-ui/core"
-import AltListItem from "./AltListItem"
-import { LoadMore } from "./LoadMore"
+import React from "react";
+import { Box, Divider, Link, Tooltip, Text, SimpleGrid } from "@chakra-ui/core";
+import AltListItem from "./AltListItem";
+import { LoadMore } from "./LoadMore";
 
-// Shuffles an array in-place using the Durstenfeld shuffle algorithm
+// Shuffles an array in-place using Durstenfeld shuffle algorithm
 const shuffle = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    const temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
-
-  return array
-}
+  return array;
+};
 
 export default function CompGroup({ comp, commercial, alts }) {
-  const shuffled = shuffle(alts)
+  const shuffled = shuffle(alts);
 
   return (
-    <Box mb={16} width={{ base: "100%", lg: "50%" }}>
-      <Box mx={{ base: 0, sm: 1 }}>
-        <Box display="flex">
-          {commercial.map((com) => (
-            <Box display="flex" alignItems="center" ml={2}>
-              <Tooltip
-                bg="rgb(45, 55, 72)"
-                color="rgba(255, 255, 255, 0.92);"
-                label={com.main}
-                placement="top"
-              >
-                <Box as="a" href={com.website} target="_blank">
-                  <Box
-                    as="img"
-                    height="3rem"
-                    minWidth="3rem"
-                    bg="white"
-                    boxShadow="md"
-                    p={1}
-                    // py={3}
-                    // px={4}
-                    borderRadius={5}
-                    src={com.svg}
-                    alt=""
-                  />
-                </Box>
-              </Tooltip>
-            </Box>
+    <Box
+      mb={12}
+      width={{ base: "100%", md: "48%" }}
+      mx={{ base: 0, md: 2 }}
+      borderRadius="lg"
+      boxShadow="lg"
+      overflow="hidden"
+      bg="white"
+      transition="transform 0.2s"
+      _hover={{ transform: "scale(1.02)" }}
+    >
+      {/* Commercial Apps */}
+      <Box p={4} borderBottom="1px solid" borderColor="gray.200">
+        <Text fontWeight="bold" mb={2} fontSize="md" color="gray.600">
+          Commercial Apps
+        </Text>
+        <Box display="flex" flexWrap="wrap">
+          {commercial.map((com, index) => (
+            <Tooltip
+              key={index}
+              label={com.main}
+              bg="gray.700"
+              color="white"
+              placement="top"
+              borderRadius="md"
+              fontSize="sm"
+            >
+              <Link href={com.website} isExternal mx={1} my={1}>
+                <Box
+                  as="img"
+                  src={com.svg}
+                  alt={com.main}
+                  height="3rem"
+                  width="3rem"
+                  borderRadius="md"
+                  boxShadow="md"
+                  transition="transform 0.2s"
+                  _hover={{ transform: "scale(1.1)" }}
+                  bg="white"
+                  p={1}
+                />
+              </Link>
+            </Tooltip>
           ))}
         </Box>
-        <Divider borderColor="gray.400" />
-        <Box display="flex" flexWrap="wrap">
-          {shuffled.length <= 5 ? (
-            shuffled.map((alt) => <AltListItem alt={alt} />)
-          ) : (
-            <LoadMore alts={shuffled} />
-          )}
-        </Box>
+      </Box>
+
+      {/* Open Source Alternatives */}
+      <Box p={4}>
+        <Text fontWeight="bold" mb={2} fontSize="md" color="gray.600">
+          Open Source Alternatives
+        </Text>
+        <Divider borderColor="gray.200" mb={3} />
+        {shuffled.length <= 5 ? (
+          <SimpleGrid columns={{ base: 1, sm: 2 }} spacing={4}>
+            {shuffled.map((alt) => (
+              <AltListItem key={alt.name} alt={alt} />
+            ))}
+          </SimpleGrid>
+        ) : (
+          <LoadMore alts={shuffled} />
+        )}
       </Box>
     </Box>
-  )
+  );
 }
