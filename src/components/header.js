8abb0ec24bccd5e6a143ed5343cf0d4fa461b2d6
OpenNovaAiLabs <jamescheriyanco@gmail.com>
2025-10-06 12:35:11 +0100

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,7 @@ import {
   PopoverBody,
 } from "@chakra-ui/core"
 import Logo from "./logo"
+import "logo.png"
 
 export default function Header({ children, location }) {
   const { colorMode, toggleColorMode } = useColorMode()
@@ -57,7 +58,10 @@ export default function Header({ children, location }) {
             <Link to="/">
               <Box display="flex" my={3} pr={2}>
                 <Box minWidth="46px" maxWidth="0px">
-                  <Logo />
+                  <img
+                      src="src/components/logo.png"
+                      style={{ height: "50px", borderRadius: "8px" }}
+                  />
                 </Box>
                 <Box>
                   <Text
@@ -78,7 +82,7 @@ export default function Header({ children, location }) {
                     lineHeight="2.3rem"
                     letterSpacing=".5px"
                   >
-                    Builders
+                    AI Apps
                   </Text>
                 </Box>
               </Box>
@@ -146,31 +150,6 @@ export default function Header({ children, location }) {
                         my: 2,
                         onClick: onClose,
                       })}
-                      <PseudoBox
-                        display="flex"
-                        alignItems="center"
-                        borderRadius={4}
-                        mx={2}
-                        px={2}
-                        py={1}
-                        as="a"
-                        href="https://github.com/junaid33/opensource.builders"
-                        target="_blank"
-                        _hover={{ color: "gray.50", bg: "#1a294c" }}
-                      >
-                        <svg
-                          stroke="#ffffff"
-                          fill="#ffffff"
-                          strokeWidth="0"
-                          version="1.1"
-                          viewBox="0 0 32 32"
-                          height="1.2em"
-                          width="1.2em"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M16 5.343c-6.196 0-11.219 5.023-11.219 11.219 0 4.957 3.214 9.162 7.673 10.645 0.561 0.103 0.766-0.244 0.766-0.54 0-0.267-0.010-1.152-0.016-2.088-3.12 0.678-3.779-1.323-3.779-1.323-0.511-1.296-1.246-1.641-1.246-1.641-1.020-0.696 0.077-0.682 0.077-0.682 1.126 0.078 1.72 1.156 1.72 1.156 1.001 1.715 2.627 1.219 3.265 0.931 0.102-0.723 0.392-1.219 0.712-1.498-2.49-0.283-5.11-1.246-5.11-5.545 0-1.226 0.438-2.225 1.154-3.011-0.114-0.285-0.501-1.426 0.111-2.97 0 0 0.941-0.301 3.085 1.15 0.894-0.25 1.854-0.373 2.807-0.377 0.953 0.004 1.913 0.129 2.809 0.379 2.14-1.453 3.083-1.15 3.083-1.15 0.613 1.545 0.227 2.685 0.112 2.969 0.719 0.785 1.153 1.785 1.153 3.011 0 4.31-2.624 5.259-5.123 5.537 0.404 0.348 0.761 1.030 0.761 2.076 0 1.5-0.015 2.709-0.015 3.079 0 0.299 0.204 0.648 0.772 0.538 4.455-1.486 7.666-5.69 7.666-10.645 0-6.195-5.023-11.219-11.219-11.219z"></path>
-                        </svg>
-                      </PseudoBox>
                     </PopoverBody>
                   </PopoverContent>
                 </>
@@ -183,29 +162,6 @@ export default function Header({ children, location }) {
             {navItem({ to: "/", name: "Alternatives" })}
             {navItem({ to: "/requests", name: "Requests" })}
             {navItem({ to: "/about", name: "About" })}
-            <PseudoBox
-              display="flex"
-              alignItems="center"
-              borderRadius={4}
-              px={2}
-              as="a"
-              href="https://github.com/junaid33/opensource.builders"
-              target="_blank"
-              _hover={{ color: "gray.50", bg: "#1a294c" }}
-            >
-              <svg
-                stroke="#ffffff"
-                fill="#ffffff"
-                strokeWidth="0"
-                version="1.1"
-                viewBox="0 0 32 32"
-                height="1.2em"
-                width="1.2em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M16 5.343c-6.196 0-11.219 5.023-11.219 11.219 0 4.957 3.214 9.162 7.673 10.645 0.561 0.103 0.766-0.244 0.766-0.54 0-0.267-0.010-1.152-0.016-2.088-3.12 0.678-3.779-1.323-3.779-1.323-0.511-1.296-1.246-1.641-1.246-1.641-1.020-0.696 0.077-0.682 0.077-0.682 1.126 0.078 1.72 1.156 1.72 1.156 1.001 1.715 2.627 1.219 3.265 0.931 0.102-0.723 0.392-1.219 0.712-1.498-2.49-0.283-5.11-1.246-5.11-5.545 0-1.226 0.438-2.225 1.154-3.011-0.114-0.285-0.501-1.426 0.111-2.97 0 0 0.941-0.301 3.085 1.15 0.894-0.25 1.854-0.373 2.807-0.377 0.953 0.004 1.913 0.129 2.809 0.379 2.14-1.453 3.083-1.15 3.083-1.15 0.613 1.545 0.227 2.685 0.112 2.969 0.719 0.785 1.153 1.785 1.153 3.011 0 4.31-2.624 5.259-5.123 5.537 0.404 0.348 0.761 1.030 0.761 2.076 0 1.5-0.015 2.709-0.015 3.079 0 0.299 0.204 0.648 0.772 0.538 4.455-1.486 7.666-5.69 7.666-10.645 0-6.195-5.023-11.219-11.219-11.219z"></path>
-              </svg>
-            </PseudoBox>
           </Box>
         </Box>
       </Box>
